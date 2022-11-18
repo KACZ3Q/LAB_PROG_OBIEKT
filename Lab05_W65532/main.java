2e4054f4098624ae9a596b38b8da67678cf6c366
KACZ3Q <115534849+KACZ3Q@users.noreply.github.com>
2022-11-18 12:08:00 +0100

    Update main.java

--- Lab05_W65532/main.java
+++ Lab05_W65532/main.java
@@ -1,1 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+       // zadanie1();
+        zadanie2();
 
+
+
+    }
+    public static void zadanie1(){
+        Prostokat p=new Prostokat(2,3);
+        System.out.println("Pole prosokata "+p.pole());
+        System.out.println("Obwod prostokata "+p.obwod());
+        System.out.println("Dlugosc przekatnej prostokata "+p.dlugoscprze());
+    }
+    public static void zadanie2(){
+        Budynek b1=new Budynek("dom",2007,2);
+        Budynek b2=new Budynek("blok",2000,6);
+        b1.info();
+        b2.info();
+
+
+    }
+}
