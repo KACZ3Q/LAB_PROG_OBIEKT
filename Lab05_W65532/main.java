5dbda3e42591be46217a740ddd1a2d31fada271f
KACZ3Q <115534849+KACZ3Q@users.noreply.github.com>
2022-11-18 12:32:48 +0100

    Update main.java

--- Lab05_W65532/main.java
+++ Lab05_W65532/main.java
@@ -17,7 +17,17 @@ public class Main {
         Budynek b2=new Budynek("blok",2000,6);
         b1.info();
         b2.info();
-
-
+    }
+    public static void zadanie3(){
+        Gatunek g1=new Gatunek("a","b",26, 2);
+        Gatunek g2=new Gatunek("a","c",56, 2);
+        Gatunek g3=g1.kopiaGatunku();
+        g1.pelnaNazwa();
+        g1.haploidalne();
+        System.out.println();
+        g2.wypisz();
+        System.out.println();
+        g1.wypisz();
+        g3.wypisz();
     }
 }
