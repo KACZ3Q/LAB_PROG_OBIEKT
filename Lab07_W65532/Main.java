4fa37009e8097e57e0b1ae7a8342881d9aed789d
KACZ3Q <115534849+KACZ3Q@users.noreply.github.com>
2022-11-22 11:08:39 +0100

    Create Main.java

---
+++ Lab07_W65532/Main.java
@@ -0,0 +1,15 @@
+class Main
+{
+	public static void main(String[] arg)
+	{
+		zad1();
+
+	}
+	public static void zad1()
+	{
+		Samolot s1=new Samolot();
+		s1.Lec();
+		Statek st1=new Statek();
+		st1.Plyn();
+	}
+}
