38d4096be84781193519e264fe380acdc1c329e5
KACZ3Q <115534849+KACZ3Q@users.noreply.github.com>
2022-12-06 11:05:04 +0100

    Update przyklad.java

--- LAB10_W65532/przyklad.java
+++ LAB10_W65532/przyklad.java
@@ -1,1 +1,84 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class przyklad extends JFrame {
+    private JPanel panel1;
+    private JButton wyjśćieButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton sumaButton;
+    private JButton różnicaButton;
+    private JRadioButton javaRadioButton;
+    private JRadioButton cRadioButton;
+    private JCheckBox poniedziałekCheckBox;
+    private JCheckBox wtorekCheckBox;
+    private JButton wyczyśćButton;
+    private JLabel WynikL;
+    private JButton wyświetlWybórButton;
+
+    public static void  main(String[] args)
+    {
+        przyklad gui = new przyklad();
+        gui.setVisible(true);
+    }
+    double a,b,wynik;
+    public przyklad()
+    {
+        super("Moje pierwsze GUI");
+        this.setContentPane(this.panel1);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        int width=400,height=400;
+        this.setSize(width,height);
+
+        wyjśćieButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        sumaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                a= Double.parseDouble(textField1.getText());
+                b= Double.parseDouble(textField2.getText());
+                wynik=a+b;
+                WynikL.setText("Suma: "+String.valueOf(a)+" + "+String.valueOf(b)+" = "+String.valueOf(wynik));
+            }
+        });
+        różnicaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                a= Double.parseDouble(textField1.getText());
+                b= Double.parseDouble(textField2.getText());
+                wynik=a-b;
+                WynikL.setText("Różnica: "+String.valueOf(a)+" - "+String.valueOf(b)+" = "+String.valueOf(wynik));
+            }
+        });
+        wyświetlWybórButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text="";
+                if (javaRadioButton.isSelected()&& poniedziałekCheckBox.isSelected()) text="JAVA - poniedziałek";
+                else if (javaRadioButton.isSelected()&& wtorekCheckBox.isSelected()) text="JAVA - wtorek";
+                else if (javaRadioButton.isSelected()) text="JAVA";
+                else if (cRadioButton.isSelected()&& poniedziałekCheckBox.isSelected()) text="C# - poniedziałek";
+                else if (cRadioButton.isSelected()&& wtorekCheckBox.isSelected()) text="C# - wtorek";
+                else if (cRadioButton.isSelected()) text="C#";
+
+                        JOptionPane.showMessageDialog(przyklad.this,"Wybrano język: "+text);
+            }
+        });
+        wyczyśćButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textField1.setText("");
+                textField2.setText("");
+                poniedziałekCheckBox.isSelected();
+                wtorekCheckBox.isSelected();
+            }
+        });
+    }
+}
 
