0c50b45de9bade69bd451f8d669544755a9b3ec8
KACZ3Q <115534849+KACZ3Q@users.noreply.github.com>
2022-12-16 16:11:23 +0100

    Create Zwierze.java

---
+++ Lab07_W65532/Zwierze.java
@@ -0,0 +1,11 @@
+public abstract class Zwierze implements  Lata{
+    @Override
+    public void Lec() {
+        System.out.println("leci");
+    }
+
+    @Override
+    public void Lad() {
+        System.out.println("laduje");
+    }
+}
